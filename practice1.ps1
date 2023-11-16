ab1014ec37d05054caf319e0982cf37eb968a87b
Anastasija Firsanova <anastasija.firsanova@voco.ee>
2023-11-16 14:28:46 +0200

    Practice1 solution

---
+++ practice1.ps1
@@ -0,0 +1,14 @@
+﻿$numbers1 = @(1,2,3)
+$numbers2 = @(4,5,6)
+
+$numbers3 += $numbers1[0] + $numbers2[0]
+$numbers3 += $numbers1[1] + $numbers2[1]
+$numbers3 += $numbers1[2] + $numbers2[2]
+
+#variant2
+
+$numbers3 = @()
+$numbers3 = $numbers3 + $el1
+$numbers3 = $numbers3 + $el2
+$numbers3 += $el3
+$numbers3
