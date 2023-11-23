6bdc7dd5ab93234ec335cc205ef84c40b75969f9
Anastasija Firsanova <anastasija.firsanova@voco.ee>
2023-11-23 14:27:44 +0200

    Lisan ülesande 2 lahenduse

---
+++ yl2.ps1
@@ -0,0 +1,20 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+if ($eesnimi -cmatch '^[A-Za-z]+$' -and $perenimi -cmatch '^[A-Za-z]+$') {
+    $kasutajanimi = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+
+    Write-Host "Kustutav kasutaja on $kasutajanimi"
+}
+
+try {
+    $ErrorActionPreference = ‘SilentlyContinue’
+    Get-LocalUser -Name $kasutajanimi
+} catch {
+}
+try {
+    Remove-LocalUser -Name $kasutajanimi
+    Write-Host "Kasutaja $kasutajanimi on kustutatud"
+} catch {
+    Write-Host "Tekkinud probleem kasutaja kustutamisega"
+}
