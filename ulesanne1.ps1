dd7d25ed0a677015a2658c20940d4b1268ec8ccf
Anastasija Firsanova <anastasija.firsanova@voco.ee>
2023-11-23 13:57:38 +0200

    Lisan ülesande 1 lahenduse

---
+++ ulesanne1.ps1
@@ -0,0 +1,17 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+if ($eesnimi -cmatch '^[A-Za-z]+$' -and $perenimi -cmatch '^[A-Za-z]+$') {
+    $kasutajanimi = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+
+    Write-Host "Loodav kasutaja on $kasutajanimi"
+}
+
+$ErrorActionPreference = ‘SilentlyContinue’
+New-LocalUser -Name $kasutajanimi -Password (ConvertTo-SecureString "Parool1!" -AsPlainText -Force)
+
+if ($?) {
+    Write-Host "Kasutaja $kasutajanimi loodi edukalt!"
+} else {
+    Write-Host "Tekkinud probleem kasutaja loomisega"
+}
